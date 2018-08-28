66fb3b6e85b8943012e0f52ab1ef4d9160c3533d
AlisaAmiella <geovane.major7@gmail.com>
2018-08-28 17:12:08 -0300

    Tarefas dia 28.08

---
+++ src/br/com/senai/teste/Tarefa6Pag32.java
@@ -0,0 +1,28 @@
+package br.com.senai.teste;
+
+import java.util.Scanner;
+public class Tarefa6Pag32 {
+	public static void main(String[] args) {
+		Scanner Megumin = new Scanner(System.in);
+		double n;
+		int m;
+		double libras = 454;
+		System.out.printf("Digite a quantidade:  \n");
+		n = Megumin.nextInt(); 
+		System.out.printf("1-para quilos e 2-para libras \n");
+		m = Megumin.nextInt();
+		switch (m){
+		case 1:
+		{
+			double a = ();
+			System.out.println("Número em quilos: "+n);
+			System.out.println("Número em libras: "+a);
+			break;
+		}
+		case 2:
+		{
+			break;
+		}
+		}
+	}
+}
